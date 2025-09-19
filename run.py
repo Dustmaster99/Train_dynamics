a04f37685c731fe04a7b9bcb30b7a839229056cf
ZEC <eos.josehenrique@gmail.com>
2025-09-19 17:33:39 -0300

    changes in the run.py to add new steps

--- run.py
+++ run.py
@@ -7,23 +7,38 @@ Created on Thu Sep  4 15:09:48 2025
 import os
 os.chdir(r'C:\Kegle_Jojo\Train_Dynamics')
 from classes.Agents import TrainFlowModel
+import classes.Generate_network as Gen
 import classes.plot as plot
 import numpy as np
 
-adj_matrix = [
-    [0, 10, 0, 0, 0, 0, 10],
-    [10, 0, 10, 0, 0, 0, 0],
-    [0, 10, 0, 10, 0, 0, 0],
-    [0, 0, 10, 0, 10, 0, 0],
-    [0, 0, 0, 10, 0, 10, 0],
-    [0, 0, 0, 0, 10, 0, 10],
-    [10, 0, 0, 0, 0, 10, 0]
-]
-n_trains = 2
+'''
+# Grafo watts_strogatz com 8 nós
+adj_matrix = Gen.gerar_matriz_adjacencia(
+    topo_type="watts_strogatz",
+    n=8,      # Número de nós
+    k=2,      # Cada nó conecta a 2 vizinhos (grau 2 para circular puro)
+    p=0.4       # Probabilidade zero de rewiring - mantém a estrutura circular
+)
+Gen.visualizar_matriz_adjacencia (adj_matrix)
+print(adj_matrix)
 
+'''
+
+adj_matrix = [
+    [0, 50, 0, 0, 0, 0, 50],
+    [50, 0, 50, 0, 0, 0, 0],
+    [0, 50, 0, 50, 0, 0, 0],
+    [0, 0, 50, 0, 50, 0, 0],
+    [0, 0, 0, 50, 0, 50, 0],
+    [0, 0, 0, 0, 50, 0, 50],
+    [50, 0, 0, 0, 0, 50, 0]
+]
+
+
+n_trains = 1
 
 model = TrainFlowModel(n_trains,adj_matrix)
-for t in range(10):
+for t in range(50):
     model.step()
     plot.print_network_state(model.grid.G,t) 
     plot.plot_network_state(model.grid.G,t)
