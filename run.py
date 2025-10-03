8b0d038b9d0902a21abe0ec5eb6ac1667c321049
ZEC <eos.josehenrique@gmail.com>
2025-10-03 17:31:48 -0300

    changes in run.py and cache

--- run.py
+++ run.py
@@ -28,24 +28,26 @@ print(adj_matrix)
 
 
 adj_matrix = [
-    [0, 50, 50, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 50, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 50, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 50, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 50, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 50, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 50, 50, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 50],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 50],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 100, 100, 0, 0, 0, 0, 0, 0, 0],  # nó 0 → nó 1 e 2
+    [0, 0, 0, 100, 0, 0, 0, 0, 0, 0],   # nó 1 → nó 3
+    [0, 0, 0, 100, 0, 0, 0, 0, 0, 0],   # nó 2 → nó 3
+    [0, 0, 0, 0, 100, 0, 0, 0, 0, 0],   # nó 3 → nó 4
+    [0, 0, 0, 0, 0, 100, 100, 100, 0, 0],   # nó 4 → nó 5, 6 e 7
+    [0, 0, 0, 0, 0, 0, 0, 0, 100, 0],   # nó 5 → nó 8
+    [0, 0, 0, 0, 0, 0, 0, 0, 100, 100], # nó 6 → nós 8 e 9
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 100],   # nó 7 → nó 9
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],   # nó 8 → (sem conexões de saída)
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]      # nó 9 (sem conexões de saída)
 ]
 
 
 number_of_nodes = 10
 
 #station_nodes = ["S_A","S_B","S_C","S_D","S_E","S_F","S_G","S_H","S_I","S_J","S_K","S_L","S_M"]
-station_nodes = ["S_A","S_B","S_C","S_D"]
-station_nodes =  Gen.insert_random_nones(station_nodes,number_of_nodes)
+#station_nodes = ["S_A","S_B","S_C","S_D"]
+#station_nodes =  Gen.insert_random_nones(station_nodes,number_of_nodes)
+station_nodes = ["S_A", None ,None,"S_B",None,None,None,None,"S_C","S_D"] 
+train_nodes = ["T_A", None ,None,None,None,None,None,None,None,"T_B"] 
 
 '''
 adj_matrix = Gen.gerar_matriz_adjacencia_2(
@@ -56,7 +58,7 @@ adj_matrix = Gen.gerar_matriz_adjacencia_2(
 
 n_trains = 1
 
-model = TrainFlowModel(n_trains,adj_matrix, station_nodes)
+model = TrainFlowModel(adj_matrix, station_nodes, train_nodes)
 for t in range(50):
     model.step()
     plot.print_network_state(model.grid.G,t) 
